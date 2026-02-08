ba000a4d844b06dd83ad4267a541f55168577db3
Dua Criativa <duacriativa@Duas-MacBook-Air.local>
2026-02-07 21:11:38 -0300

    Install Google Analytics (GA4)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google"; // Changed from Geist to Montserrat
+import Script from "next/script";
 import "./globals.css";
 
 const montserrat = Montserrat({
@@ -23,6 +24,20 @@ export default function RootLayout({
       <body
         className={`${montserrat.variable} antialiased`}
       >
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-1TF1L4L29R"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-1TF1L4L29R');
+          `}
+        </Script>
         {children}
       </body>
     </html>
