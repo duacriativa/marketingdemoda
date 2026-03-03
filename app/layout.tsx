ea9f2ff82a938a39764f2cf66be56d5c79acbbc5
Cliente Dua Criativa <cliente@duacriativa.com>
2026-03-03 14:33:33 -0300

    feat: install Google Tag Manager GTM-T9WQ7M6S

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,16 @@ export default function RootLayout({
       <body
         className={`${montserrat.variable} antialiased`}
       >
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T9WQ7M6S');
+          `}
+        </Script>
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-1TF1L4L29R"
@@ -40,6 +50,14 @@ export default function RootLayout({
           `}
         </Script>
         <FacebookPixel />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T9WQ7M6S"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <noscript>
           <img
             height="1"
