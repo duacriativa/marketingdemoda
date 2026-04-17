7c73621aa4602b3983fb631ea6b314022bb215f1
Dua Criativa <duacriativa@Duas-MacBook-Air.local>
2026-04-17 20:16:05 -0300

    fix: restore GA4 tracking and fix GTM syntax error

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,16 +28,29 @@ export default function RootLayout({
       <body
         className={`${montserrat.variable} antialiased`}
       >
-        {/* Google Tag Manager */}
+        {/* Google Tag Manager - Fixed JS Syntax Error + Direct Analytics */}
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script''dataLayer','GTM-T9WQ7M65');
+            })(window,document,'script','dataLayer','GTM-T9WQ7M65');
           `}
         </Script>
+        {/* Google Analytics GA4 */}
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1TF1L4L29R');
+          `}
+        </Script>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-1TF1L4L29R"
+          strategy="afterInteractive"
+        />
         <Script
           id="microsoft-clarity"
           strategy="afterInteractive"
